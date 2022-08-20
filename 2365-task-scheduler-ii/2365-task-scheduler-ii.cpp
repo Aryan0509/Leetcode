6beeef7b5cb0fdfc69d19ca1d78a7eb3c67f67a3
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-08-21 01:27:35 +0530

    Time: 511 ms (22.87%), Space: 100.1 MB (66.71%) - LeetHub

---
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long taskSchedulerII(vector<int>& tasks, int space) {
+        map<int,long long>m;
+        long long days=0;
+        int n=tasks.size();
+        int i=0;
+        while(i<n)
+        {
+            if(m[tasks[i]]<=days)
+            {
+                m[tasks[i]]=days+space+1;
+                i++;
+                days++;
+                continue;
+            }
+            else{
+                days=m[tasks[i]];
+                m[tasks[i]]=days+space+1;
+                i++;
+                days++;
+                
+            }
+            
+        }
+        return days;
+    }
+};
