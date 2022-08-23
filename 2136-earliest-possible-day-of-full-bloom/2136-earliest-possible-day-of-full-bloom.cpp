2dfc91c69decc43cea7a327c0001797a29ddadb5
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-08-23 23:55:26 +0530

    Time: 669 ms (22.00%), Space: 168.7 MB (37.40%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    static bool cmp(pair<int,int>p1,pair<int,int>p2)
+    {
+        if(p1.second==p2.second)
+            return p1.first<p2.first;
+       return p1.second>p2.second;
+    }
+    
+    int earliestFullBloom(vector<int>& plant, vector<int>& grow) {
+        vector<pair<int,int>>p;
+        int n=plant.size();
+        for(int i=0;i<n;i++)
+        {
+            p.push_back({plant[i],grow[i]+1});
+        }
+        sort(p.begin(),p.end(),cmp);
+        int days=-1;
+        int maxx=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            days+=p[i].first;
+            maxx=max(maxx,days+p[i].second);
+            // cout<<days<<" "<<maxx<<endl;
+        }
+            return maxx;
+ 
+        
+    }
+};
