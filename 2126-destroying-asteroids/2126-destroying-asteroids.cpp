045bc0737345c51824caefdaad02152a406640bd
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-08-23 17:56:39 +0530

    Time: 341 ms (51.33%), Space: 102.7 MB (92.47%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(long long mass, vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]>mass)
+                return false;
+            mass+=arr[i];
+        }
+        return true;
+    }
+};
