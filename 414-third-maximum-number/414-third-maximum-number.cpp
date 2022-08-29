ae6030b52a66a26f12b8ef23e6ceb6ecb70a5544
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-08-29 20:49:21 +0530

    Time: 27 ms (6.48%), Space: 10.7 MB (27.02%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -3,25 +3,9 @@ public:
     int thirdMax(vector<int>& nums) {
         set<int>s(nums.begin(),nums.end());
         nums.assign(s.begin(),s.end());
-        priority_queue<int,vector<int>,greater<int>>q;
-        for(auto it:nums)
-        {
-            if(q.size()<3)
-            {
-                q.push(it);
-            }
-            else{
-                if(it>q.top())
-                {
-                    q.pop();
-                    q.push(it);
-                }
-            }
-        }
-        if(q.size()<3)
-        {
-           return *max_element(nums.begin(),nums.end());
-        }
-        return q.top();
+        sort(nums.begin(),nums.end(),greater<int>());
+        if(nums.size()>=3)
+            return nums[2];
+        return nums[0];
     }
 };
