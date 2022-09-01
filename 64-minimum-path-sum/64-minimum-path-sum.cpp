14af1afae38d95fe7aa92559c191ed25e44a1b9d
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-09-02 00:09:26 +0530

    Time: 20 ms (28.24%), Space: 10.1 MB (55.94%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -6,15 +6,16 @@ public:
         {
             return INT_MAX/2;
         }
+        if(dp[r][c]!=-1)
+        {
+            return dp[r][c];
+        }
         if(r==n-1 and c==m-1)
         {
             dp[r][c]=grid[r][c];
             return grid[r][c];
         }
-        if(dp[r][c]!=-1)
-        {
-            return dp[r][c];
-        }
+       
         int o1=solve(grid,dp,r+1,c,n,m);
         int o2=solve(grid,dp,r,c+1,n,m);
         dp[r][c]=grid[r][c]+min(o1,o2);
