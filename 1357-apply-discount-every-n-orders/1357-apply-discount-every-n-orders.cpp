d98b5993b7d35b9c598f14f1e7cda8fa51813b2e
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-09-08 23:55:52 +0530

    Time: 476 ms (25.44%), Space: 120.7 MB (7.02%) - LeetHub

---
+++ 1357-apply-discount-every-n-orders/1357-apply-discount-every-n-orders.cpp
@@ -0,0 +1,38 @@
+class Cashier {
+public:
+    int count;
+    int nn;
+    map<int,int>m;
+    int disc;
+    Cashier(int n, int discount, vector<int>& products, vector<int>& prices) {
+        count=0;
+        nn=n;
+        int x=products.size();
+        for(int i=0;i<x;i++)
+        {
+            m[products[i]]=prices[i];
+        }
+        disc=discount;
+    }
+    
+    double getBill(vector<int> product, vector<int> amount) {
+        count++;
+        double bill=0;
+        int x=product.size();
+        for(int i=0;i<x;i++)
+        {
+            bill+=m[product[i]]*amount[i];
+        }
+        if(count%nn==0)
+        {
+            bill=bill*(double(100-disc)/double(100));
+        }
+        return bill;
+    }
+};
+
+/**
+ * Your Cashier object will be instantiated and called as such:
+ * Cashier* obj = new Cashier(n, discount, products, prices);
+ * double param_1 = obj->getBill(product,amount);
+ */
