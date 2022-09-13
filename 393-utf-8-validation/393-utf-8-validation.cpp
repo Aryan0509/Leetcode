0a2a1ae1345f9095c0e2b00a3fc89474cef6c64f
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-09-13 22:23:41 +0530

    Time: 158 ms (5.24%), Space: 13.9 MB (76.31%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,83 @@
+class Solution {
+public:
+    
+    
+    bool check(vector<int>& data,int s,int len)
+    {
+        s++;
+        len--;
+        while(len--)
+        {
+            string x=convert(data[s++]);
+            if(x[0]!='1' or x[1]!='0')
+                return false;
+        }
+        return true;
+    }
+    string convert(int n)
+    {
+        string x="00000000";
+        if(n==0)
+            return x;
+        cout<<n<<" ";
+        int ptr=7;
+        while(n!=0)
+        {
+            x[ptr--]=n%2+'0';
+            n=n/2;
+        }
+        cout<<x<<endl;
+        return x;
+    }
+    int count(vector<int>&data,int s)
+    {
+        int n=data[s];
+        string x=convert(n);
+        int i=0;
+        int countt=0;
+        while(x[i]=='1')
+        {
+            countt++;
+            i++;
+        }
+        return countt;
+    }
+    bool validUtf8(vector<int>& data) {
+        int n=data.size();
+        if(data.size()==1)
+        {
+           string x=convert(data[0]);
+            if(x[0]=='0')
+                return true;
+            return false;
+        }
+        int i=0;
+        while(i<n)
+        {
+            int len=count(data,i);
+            if(len==0)
+            {
+                i++;
+                
+            }
+            else if(i+len-1>=n or len>=5)
+                return false;
+            else if(len==1)
+            {
+                return false;
+            }
+            else{
+                if(!check(data,i,len))
+                {
+                    cout<<"HEY"<<i<<endl;
+                    return false;
+                }  
+                else {
+                    i+=len;
+                }
+            }
+        }
+        return true;
+        
+    }
+};
