9c61e5e1b47c72d9ec543bafa014f110ee9b7587
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-10-04 16:20:51 +0530

    Time: 120 ms (5.00%), Space: 13.4 MB (69.37%) - LeetHub

---
+++ 539-minimum-time-difference/539-minimum-time-difference.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    static bool cmp(string s1,string s2)
+    {
+        if(stoi(s1.substr(0,2))==stoi(s2.substr(0,2)))
+            return stoi(s1.substr(3,2))<stoi(s2.substr(3,2));
+        return stoi(s1.substr(0,2))<stoi(s2.substr(0,2));
+    }
+    int findMinDifference(vector<string>& tp) {
+        sort(tp.begin(),tp.end(),cmp);
+        int mind=INT_MAX;
+        int n=tp.size();
+        for(int i=0;i<n-1;i++)
+        {
+            int currentmin=0;
+            int ch=stoi(tp[i].substr(0,2));
+            // cout<<ch<<endl;
+            int th=stoi(tp[i+1].substr(0,2));
+            // cout<<th<<endl;
+            int cm=stoi(tp[i].substr(3,2));
+            // cout<<cm<<endl;
+            int tm=stoi(tp[i+1].substr(3,2));
+            // cout<<tm<<endl;
+            currentmin+=(th-ch)*60;
+            currentmin=currentmin-cm+tm;
+            mind=min(mind,currentmin);
+        }
+        cout<<mind<<endl;
+        int currentmin=0;
+            int ch=stoi(tp[n-1].substr(0,2));
+            int th=stoi(tp[0].substr(0,2));
+            int cm=stoi(tp[n-1].substr(3,2));
+            int tm=stoi(tp[0].substr(3,2));
+            currentmin+=(24+(th-ch))*60;
+            currentmin=currentmin-cm+tm;
+            mind=min(mind,currentmin);
+        return mind;
+    }
+};
