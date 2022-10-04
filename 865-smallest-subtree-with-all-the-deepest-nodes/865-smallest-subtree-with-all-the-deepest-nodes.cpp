e9a3f9737c6127b3b5160b9e96579eca183c361c
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-10-05 01:56:45 +0530

    Time: 25 ms (6.19%), Space: 16.6 MB (7.67%) - LeetHub

---
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int calcheight(TreeNode*root, map<TreeNode*,int>&height)
+    {
+        if(root==NULL)
+        {
+            height[root]=0;
+            return 0;
+        }
+        height[root]=1+max(calcheight(root->left,height),calcheight(root->right,height));
+        return height[root];
+
+    }
+    TreeNode* findsub(TreeNode*root, map<TreeNode*,int>&height)
+    {
+        if(root==NULL)
+            return root;
+        if(height[root->left]==height[root->right])
+            return root;
+        if(height[root->left]>height[root->right])
+        {
+            return findsub(root->left,height);
+        }
+        if(height[root->left]<height[root->right])
+        {
+            return findsub(root->right,height);
+        }
+        return root;
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        map<TreeNode*,int>height;
+        int h=calcheight(root,height);
+        return findsub(root,height);
+    }
+};
