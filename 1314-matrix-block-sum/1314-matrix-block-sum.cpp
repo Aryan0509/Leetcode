5b067026320bad3efbe74d50e465bfa1a18d3c23
Aryan0509 <87501633+Aryan0509@users.noreply.github.com>
2022-10-09 22:45:56 +0530

    Time: 1417 ms (11.90%), Space: 9.3 MB (73.36%) - LeetHub

---
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                for(int r=max(0,i-k);r<=min(n-1,i+k);r++)
+                {
+                  
+                        for(int c=max(0,j-k);c<=min(j+k,m-1);c++)
+                        {
+                            
+                                ans[i][j]+=mat[r][c];
+                            
+                        }
+                    
+                    
+                }
+            }
+        }
+        return ans;
+    }
+};
